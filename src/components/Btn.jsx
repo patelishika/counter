b515a60125619796cf58e70bb75979f814f71994
Ishika Patel <ishikapatel1364@gmail.com>
2025-01-21 21:35:32 +0530

    ui modify

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -2,7 +2,7 @@ const Btn = ({ name, fun }) => {
   return (
     <>
       <button
-        className='text-4xl w-32 bg-purple-400 border border-purple-700 rounded p-3 m-2'
+        className='text-4xl w-32  bg-purple-300 border border-purple-700 rounded p-3 m-2 hover:bg-purple-400 shadow-md shadow-purple-300 hover:scale-110 transition-all duration-100'
         onClick={fun}>
         {name}
       </button>
